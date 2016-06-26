a1aca8756cf441608f664c70e8f1331191b20450
Pedro Abel <angeluz-abel@hotmail.es>
2016-06-26 16:40:57 -0500

    practica 9
    arregllos

---
+++ Practica9/src/general/Principal.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package general;
+
+/**
+ *
+ * @author Angeluz
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int notas[]=new int[10];
+        notas[0]=100;
+        notas[notas.length-1]=80;
+        System.out.println(notas.length);
+    }
+    
+}
